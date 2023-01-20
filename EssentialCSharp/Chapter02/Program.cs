bcffdcc3172f4f6c2bcbff45d80d8bbd8a6bbdd5
kangcoding <kcm1014@gmail.com>
2023-01-20 16:31:58 +0900

    chapter02 complete

---
+++ EssentialCSharp/Chapter02/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+#nullable disable
+string firstName = "kang";
+string lastName = "Coding";
+
+
+Console.WriteLine(string.Format("Your full name is {0} {1}.",firstName,lastName));
+
+
+int a = (int)123L;
+int c = System.Convert.ToInt32(123L);
