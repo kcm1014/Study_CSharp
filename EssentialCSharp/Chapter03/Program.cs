af5b3651bbce93eba133f33ae7b3bbad7792bdd7
kangcoding <kcm1014@gmail.com>
2023-01-23 14:56:48 +0900

    test

--- EssentialCSharp/Chapter03/Program.cs
+++ EssentialCSharp/Chapter03/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+(string country, string capital, double gdpPerCaital) = ("South Sudan","Juba",275.18);
+(string country, string capital, double gdpPerCaital) countryInfo = ("South Sudan","Juba",275.18);
+
+Console.WriteLine($"{country} {capital} {gdpPerCaital}");
+Console.WriteLine($"{countryInfo.country} {countryInfo.capital} {countryInfo.gdpPerCaital}");
