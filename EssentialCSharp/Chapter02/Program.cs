fe13ee299d1418f226f6bc5f061c74f9b7e96757
kangcoding <kcm1014@gmail.com>
2023-01-23 14:10:43 +0900

    macbook edit

--- EssentialCSharp/Chapter02/Program.cs
+++ EssentialCSharp/Chapter02/Program.cs
@@ -5,6 +5,8 @@ string lastName = "Coding";
 
 
 Console.WriteLine(string.Format("Your full name is {0} {1}.",firstName,lastName));
+Console.WriteLine($"Your full name is {firstName} {lastName}");
+//Console.WriteLine("Your full name {0} {1}".Format(firstName,lastName));
 
 
 int a = (int)123L;
