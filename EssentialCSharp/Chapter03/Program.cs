9c33716c94ab0c2f99a77330bc4511910fa3f4d5
kangcoding <kcm1014@gmail.com>
2023-01-26 10:05:46 +0900

    Chapter03 complete

--- EssentialCSharp/Chapter03/Program.cs
+++ EssentialCSharp/Chapter03/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 (string country, string capital, double gdpPerCaital) = ("South Sudan","Juba",275.18);
 (string country, string capital, double gdpPerCaital) countryInfo = ("South Sudan","Juba",275.18);
+var countryInfo2 = (Name:"South Korea",Capital:"Seoul",gdpPerCaital:40000);
 
 Console.WriteLine($"{country} {capital} {gdpPerCaital}");
 Console.WriteLine($"{countryInfo.country} {countryInfo.capital} {countryInfo.gdpPerCaital}");
+
+Console.WriteLine($"{countryInfo2.Name} {countryInfo2.Capital} {countryInfo2.gdpPerCaital}");
+Console.WriteLine($"{countryInfo2.Item1} {countryInfo2.Item2} {countryInfo2.Item3}");
+
